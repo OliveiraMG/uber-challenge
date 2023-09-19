a8950be80fe47ced05b349c6625f938434eb6812
OliveiraMG <matheus.moura@sptech.school>
2023-09-19 15:49:02 -0300

    feat: criando camada de application

---
+++ email-service/src/main/java/com/example/emailservice/application/EmailSenderService.java
@@ -0,0 +1,19 @@
+package com.example.emailservice.application;
+
+import com.example.emailservice.adapters.EmailSenderGateaway;
+import com.example.emailservice.core.EmailSenderUseCase;
+
+public class EmailSenderService implements EmailSenderUseCase {
+
+    // A email sender gateaway é uma classe criada para ser o portão de entrada para o mundo externo e poder enviar o email. Será uma interface que é um adapter para o mundo externo, assim não haverá a dependência direta do service, assim ao criar a classe da AWS ela irá implementar a minha classe que eu defini.
+    private final EmailSenderGateaway emailSenderGateaway;
+
+    public EmailSenderService(EmailSenderGateaway emailSenderGateaway) {
+        this.emailSenderGateaway = emailSenderGateaway;
+    }
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateaway.sendEmail(to, subject, body);
+    }
+
+}

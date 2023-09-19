2412e5ae2d979af222910101f3a7f5f7771bd455
OliveiraMG <matheus.moura@sptech.school>
2023-09-19 15:49:23 -0300

    feat: criando camada de infra do serviço amazon

---
+++ email-service/src/main/java/com/example/emailservice/infra/ses/SesEmailSender.java
@@ -0,0 +1,34 @@
+package com.example.emailservice.infra.ses;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
+import com.example.emailservice.adapters.EmailSenderGateaway;
+import com.example.emailservice.core.exceptions.EmailServiceException;
+
+public class SesEmailSender implements EmailSenderGateaway {
+
+    private final AmazonSimpleEmailService amazonSimpleEmailService;
+
+    public SesEmailSender(AmazonSimpleEmailService amazonSimpleEmailService) {
+        this.amazonSimpleEmailService = amazonSimpleEmailService;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SendEmailRequest request = new SendEmailRequest()
+                .withSource("theus_oliveiramoura@hotmail.com")
+                .withDestination(new Destination().withToAddresses(to))
+                .withMessage(new Message()
+                        .withSubject(new Content(subject))
+                        .withBody(new Body().withText(new Content(body)))
+                );
+
+        try {
+            this.amazonSimpleEmailService.sendEmail(request);
+        } catch (AmazonServiceException exception) {
+            throw new EmailServiceException("Failure while sending email", exception);
+        }
+
+    }
+}

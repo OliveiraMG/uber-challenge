136e2b0c7fbdec0bcce4c736e57fee67f44833ad
OliveiraMG <matheus.moura@sptech.school>
2023-09-19 16:04:20 -0300

    feat: Adicionando o funcionamento da controller

--- email-service/src/main/java/com/example/emailservice/controllers/EmailSenderController.java
+++ email-service/src/main/java/com/example/emailservice/controllers/EmailSenderController.java
@@ -1,4 +1,31 @@
 package com.example.emailservice.controllers;
 
+import com.example.emailservice.application.EmailSenderService;
+import com.example.emailservice.core.EmailRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
 public class EmailSenderController {
+
+    private final EmailSenderService emailSenderService;
+
+    public EmailSenderController(EmailSenderService emailSenderService) {
+        this.emailSenderService = emailSenderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
+        try {
+            this.emailSenderService.sendEmail(request.to(), request.subject(), request.body());
+            return ResponseEntity.ok("Email sent successfully");
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body("Failure while sending email");
+        }
+    }
+
 }
